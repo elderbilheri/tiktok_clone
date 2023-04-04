85fb111df0cb476d5fd61295c6c273a7b4cc310d
Elder Bilheri <elderbilheri@gmail.com>
2023-04-04 04:18:05 -0300

    att hosting e arquivos firebase

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ import db from "./config/firebase";
 import { collection, getDocs } from "firebase/firestore/lite";
 
 function App() {
+  let maxHeight;
+  if (window.innerHeight <= 740) {
+    maxHeight = window.innerHeight;
+  }
+
   const [video, setVideos] = useState([]);
 
   async function getVideos() {
@@ -19,7 +24,7 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
+    <div className="App" style={{ maxHeight: maxHeight + "px" }}>
       <div className="app_videos">
         {video.map((item) => {
           return (
